cffb07137a47d4a6e46f5b5b4fb9525037d6ef3c
kupriianchuk <kupriianchuk@breeze-soft.com>
2020-01-17 15:45:58 +0200

    git

---
+++ LICENSE.php
@@ -0,0 +1,18 @@
+
+
+Settings(Page)
+  Settings service
+Billing(page)
+Billing/invoice(service)
+PrintingService
+change personalInfo
+change email
+TeacherService
+Teachers(Page)
+StudentService
+Students(Page)
+Students(Page)withId
+AdminLicences(page)
+EditLicence(page)
+AddLicence(Page)
+
